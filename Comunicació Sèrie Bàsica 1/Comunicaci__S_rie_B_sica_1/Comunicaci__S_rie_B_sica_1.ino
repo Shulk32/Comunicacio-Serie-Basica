135d28586d453ca49b120a132774890c28801189
Adrian Navarro 2017 <navarro.adrian2@gmail.com>
2018-03-15 13:11:02 +0100

    Primera pujada

---
+++ Comunicació Sèrie Bàsica 1/Comunicaci__S_rie_B_sica_1/Comunicaci__S_rie_B_sica_1.ino
@@ -0,0 +1,34 @@
+/******************************************************************* 
+**                                                                **
+**                            TITOL:                              **
+**                                                                **
+**                                                                **               
+**  Nom: Adrian Navarro    oooO 
+                          (....)     Oooo
+                           ...(     (....)
+                            ._)      )../
+                                     (_/ 
+                        Solo pasaba por aquí 
+                              ¡Saludos!       Data:               **                                                                   
+*******************************************************************/
+//************************** INCLUDE *******************************
+
+
+//************************* VARIEBLES ******************************
+
+
+
+//*************************** SETUP ********************************
+void setup()                // run once, when the sketch starts
+{
+  Serial.begin(9600);       // set up Serial library at 9600 bps
+  Serial.println("Hello world!"); // prints Hello world! with brake line
+}
+//*************************** LOOP *********************************
+void loop()                     
+{
+  
+}
+//************************* FUNCIONES ******************************
+
+//Aquest programa diu Hello world 1 vegada en la consola de port Sèrie
